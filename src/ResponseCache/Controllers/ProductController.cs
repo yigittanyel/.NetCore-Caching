49760fc14e62a0b56bcee767f005883a3a3dbbd6
Yiğit Tanyel <yigittanyel@trex.com.tr>
2023-10-02 10:28:17 +0300

    Response caching completed.

---
+++ src/ResponseCache/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ResponseCache.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet]
+        [ResponseCache(CacheProfileName= "Duration10")]
+        public IActionResult Get()
+        {
+            return Ok("Response Cache");
+        }
+    }
+}
---
+++ src/ResponseCache/Program.cs
@@ -0,0 +1,43 @@
+using DataAccess.ApplicationDbContext;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+// Response Cache
+builder.Services.AddControllersWithViews(options =>
+     options.CacheProfiles.Add("Duration10", new CacheProfile
+     {
+         Duration = 10 //10 sec
+     })
+);
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
