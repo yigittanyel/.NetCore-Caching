b5735c4ab8d4f1539347c9e96aeca9f1254c4182
Yiğit Tanyel <yigittanyel@trex.com.tr>
2023-10-02 09:48:55 +0300

    Redis cache implementation completed.

---
+++ src/DistributedCache/Services/Cache/RedisService.cs
@@ -0,0 +1,18 @@
+﻿using StackExchange.Redis;
+
+namespace DistributedCache.Services.Cache;
+
+public class RedisService
+{
+    private readonly ConnectionMultiplexer _connectionMultiplexer;
+
+    public RedisService(string url)
+    {
+        _connectionMultiplexer = ConnectionMultiplexer.Connect(url);
+    }
+
+    public IDatabase GetDb(int db)
+    {
+        return _connectionMultiplexer.GetDatabase(db);
+    }
+}
---
+++ src/DistributedCache/Services/Product/ProductService.cs
@@ -0,0 +1,38 @@
+﻿using DataAccess.ApplicationDbContext;
+using DistributedCache.Services.Cache;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+
+namespace DistributedCache.Services.Product;
+
+public class ProductService : IProductService
+{
+    private readonly AppDbContext _dbContext;
+    private readonly RedisService _redisService;
+    public ProductService(AppDbContext dbContext, RedisService redisService)
+    {
+        _dbContext = dbContext;
+        _redisService = redisService;
+    }
+
+    public async Task<List<DataAccess.Models.Product>> GetProducts()
+    {
+        string cacheKey = "productss";
+
+        var redisDb = _redisService.GetDb(0);
+        var productsJson = await redisDb.StringGetAsync(cacheKey);
+
+        if (productsJson.HasValue)
+        {
+            var products = JsonConvert.DeserializeObject<List<DataAccess.Models.Product>>(productsJson);
+            return products;
+        }
+        else
+        {
+            var products = await _dbContext.Products.ToListAsync();
+            await redisDb.StringSetAsync(cacheKey, JsonConvert.SerializeObject(products));
+
+            return products;
+        }
+    }
+}
