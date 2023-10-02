077a3dd85952252a30033f71f3bab30aaf177a50
Yiğit Tanyel <yigittanyel@trex.com.tr>
2023-10-02 10:45:14 +0300

    Output cache implementation completed.

---
+++ src/OutputCache/Controllers/ProductController.cs
@@ -0,0 +1,30 @@
+﻿using DataAccess.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.OutputCaching;
+
+namespace OutputCache.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        [HttpGet("[action]")]
+        [OutputCache(PolicyName = "CacheForTenSeconds")]
+        public IEnumerable<Product> GetFiveProduct() // 10 sec waiting time
+        {
+            return Enumerable.Range(1, 5).Select(index => new Product
+            {
+                Id = index,
+                Name = $"Product {index}",
+                Price = 100 * index     
+            })
+            .ToArray();
+        }
+
+        [HttpGet("[action]")]
+        public ActionResult<string> GetOk(string message) //default 5 sec waiting time
+        {
+            return message;
+        }
+    }
+}
